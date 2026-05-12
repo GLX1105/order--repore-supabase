4fcb286df43d0855cfa162c672b363176d43eaf8
GLX1105 <562761508@qq.com>
2026-05-13 03:45:32 +0800

    Delete cloud-functions/api/hello.js

--- cloud-functions/api/hello.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({
-    message: '后端启动成功！',
-    time: new Date().toISOString()
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
